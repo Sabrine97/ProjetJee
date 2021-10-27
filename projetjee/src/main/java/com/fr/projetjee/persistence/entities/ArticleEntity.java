358627cda36ab0b1fdbe8cd1b1ff5396ae5c0d4b
Alex <alexandraamyhall@gmail.com>
2021-10-27 15:04:44 +0200

    Refactoring de la classe ArticleEntity
    pour que Sonar arrête de me harceler -_-

---
+++ projetjee/src/main/java/com/fr/projetjee/persistence/entities/ArticleEntity.java
@@ -0,0 +1,55 @@
+package com.fr.projetjee.persistence.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class ArticleEntity implements Serializable{
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String nom;
+
+    private Date expDate;
+
+    private Date fabDate; 
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Date getExpDate() {
+        return this.expDate;
+    }
+
+    public void setExpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+
+    public Date getFabDate() {
+        return this.fabDate;
+    }
+
+    public void setFabDate(Date fabDate) {
+        this.fabDate = fabDate;
+    }
+
+
+}
--- projetjee/src/main/java/com/fr/projetjee/persistence/repository/ArticleRepository.java
+++ projetjee/src/main/java/com/fr/projetjee/persistence/repository/ArticleRepository.java
@@ -1,6 +1,6 @@
 package com.fr.projetjee.persistence.repository;
 
-import com.fr.projetjee.persistence.entitie.ArticleEntity;
+import com.fr.projetjee.persistence.entities.ArticleEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
